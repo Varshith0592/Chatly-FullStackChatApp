3053c31f3bb5ed2bc152c16fbe57f54bdd31bc6c
Varshith0592 <mannepallisaivarshith@gmail.com>
2025-06-14 04:56:41 +0530

    #

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,6 @@ import {app,server} from  './lib/socket.js'
 dotenv.config()
 
 const PORT = process.env.PORT
-const __dirname=path.resolve()
 
 
 app.use(bodyParser.json({ limit: '10mb' }))
@@ -23,21 +22,13 @@ app.use(express.json())
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: process.env.CLIENT_URL,
+  credentials: true,
 }))
 
 
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
-
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')))
-
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
-    })
-}
 
 server.listen(PORT, () => {
     console.log('Server is running on port', PORT)
